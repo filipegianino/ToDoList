3dda824c633bd94175a01d483c89296c2e641be3
Filipe Gianino <filipegianinom@gmail.com>
2023-09-22 10:26:52 -0300

    final presets

---
+++ src/components/footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from './footer.css'
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>
+        <span>React + TS ToDo</span> @ 2023
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
